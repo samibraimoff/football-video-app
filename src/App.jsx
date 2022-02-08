eda941aee8be4a045e181ac407008be3b86a9016
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-08 15:12:46 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoList from './components/VideoList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <VideoList />
+    </div>
+  );
+};
+
+export default App;
